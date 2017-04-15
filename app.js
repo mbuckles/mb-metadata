07da69edb84756b3862075e31535ee0db8bcd2c5
mbuckles <bucklesm21@gmail.com>
2017-04-15 12:01:47 -0400

    update

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ var path = require('path');
 var app = module.exports = express();
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //connect to the database
-mongoose.connect('mongo ds161580.mlab.com:61580/mb-metadata -u mbuckles -p adjf1963' || 'mongo://localhost/uploads');
+//mongoose.connect('mongo ds161580.mlab.com:61580/mb-metadata -u mbuckles -p adjf1963' || 'mongo://localhost/uploads');
 //var users = require('./routes/users');
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
+//app.use(express.static(path.join(__dirname, 'views')));
 app.use('/', index);
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 //get files
 //app.get('/', function(req, res, next){
 //  res.sendFile(__dirname + '/index.jade');
